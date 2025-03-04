daed4bebce4de222fcf4e99c41910422b02a8ad3
Diego Santos Muñoz <diegosantosmunoz@gmail.com>
2025-03-04 19:52:40 +0100

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -30,7 +30,7 @@ if [ "$Respuesta" = "si" ]; then
     	echo "Eliminando carpetas carpetas..."
 	for usuario in /home/*; do
         	rm -rf "$usuario/$NOMBRE_CARPETA"
-                ALIAS_CMD="alias uninstall-sense='$usuario/SENSE/Program_Files/uninstall.sh'"
+                ALIAS_CMD="alias uninstall-sense='bash $usuario/SENSE/Program_Files/uninstall.sh'"
 
                 if grep -Fxq "$ALIAS_CMD" /etc/bash.bashrc; then
                         sed -i "\|$ALIAS_CMD|d" /etc/bash.bashrc
