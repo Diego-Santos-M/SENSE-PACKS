972e853b9244de9450189cb74481c8b80047dcf0
Diego Santos Muñoz <diegosantosmunoz@gmail.com>
2025-03-07 12:05:15 +0100

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -10,24 +10,25 @@ for usuario in /home/*; do
 done
 
 if [ $EXISTE -eq 0 ]; then
-    echo "Error: No se ha instalado SENSE."
+    echo "Error: SENSE is not installed."
     exit 1
 fi
 
 Salir=0
 while [ $Salir -eq 0 ]; do
-    	echo "¿Estás seguro que quieres desinstalar SENSE? (si/no)"
+    	echo "Are you sure you want to uninstall SENSE (y/n)?”"
     	read Respuesta
     	Respuesta=$(echo "$Respuesta" | tr 'A-Z' 'a-z')  # Convertir a minúsculas
-    	if [[ "$Respuesta" == "si" || "$Respuesta" == "no" ]]; then
+    	if [[ "$Respuesta" == "y" || "$Respuesta" == "n" ]]; then
         	Salir=1
     	else
-        	echo "Solo se admite si/no"
+        	echo "Only y/n is allowed"
     	fi
 done
 
-if [ "$Respuesta" = "si" ]; then
-    	echo "Eliminando carpetas carpetas..."
+if [ "$Respuesta" = "y" ]; then
+    	echo "Deleting folders, subfolders, files and images....."
+	echo "Removing aliases...."
 	for usuario in /home/*; do
         	rm -rf "$usuario/$NOMBRE_CARPETA"
                 ALIAS_CMD="alias uninstall-sense='sudo bash $usuario/SENSE/Program_Files/uninstall.sh'"
@@ -35,16 +36,14 @@ if [ "$Respuesta" = "si" ]; then
 
                 if grep -Fxq "$ALIAS_CMD" /etc/bash.bashrc; then
                         sudo sed -i "\|$ALIAS_CMD|d" /etc/bash.bashrc
-                        echo "Alias eliminado de /etc/bash.bashrc"
                 fi
 		if grep -Fxq "$ALIAS_CMD_SENSE" /etc/bash.bashrc; then
         		sudo sed -i "\|$ALIAS_CMD_SENSE|d" /etc/bash.bashrc
-        		echo "Alias SENSE eliminado de /etc/bash.bashrc para el usuario $usuario"
     		fi
 	done
-        echo "Las carpetas han sido eliminadas"
+        echo "SENSE has been uninstalled"
 
-elif [ "$Respuesta" = "no" ]; then
-    	echo "Desinstalación cancelada."
+elif [ "$Respuesta" = "n" ]; then
+    	echo "Uninstallation cancelled."
 fi
 
